4ed0eb8d166a4785d485b1fecc9ea3d5188749e0
Bharat Dhaker <bharatkumardhaker@gmail.com>
2022-06-15 10:09:00 +0530

    add: config....

--- login.php
+++ login.php
@@ -8,7 +8,10 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 if( !empty($email) && !empty($password)){
-  $sql = "select id, name, email,password, status, role from users where email = '$email' LIMIT 1";
+
+
+echo  $sql = "select id, name, email,password, status, role from users where email = '$email' LIMIT 1";
+die();
 
   $stmt = $conn->prepare("select id, name, email,password, status, role from users where email = ? LIMIT 1");
   $stmt->bind_param("s", $email);
