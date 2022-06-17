e37d4660205de9371fb460eccf6a250c221231d8
Bharat Dhaker <bharatkumardhaker@gmail.com>
2022-06-17 17:37:14 +0530

    plans check plan_id

--- plans-purchase.php
+++ plans-purchase.php
@@ -9,41 +9,47 @@ $qty = $_POST['qty'];
 
 $plan_id = $_POST['plan_id'];
 
-$price_data = $conn->query("select price_id, role_name from plans where id=$plan_id");
-if($price_data ){
+if ($qty && $plan_id) {
 
-$row = $price_data->fetch_assoc();
-$price_id = $row['price_id'];
-$role_name = $row['role_name'];
-$user_id = $_SESSION['user_id'];
+    $price_data = $conn->query("select price_id, role_name from plans where id=$plan_id");
+    if ($price_data) {
 
-$checkout_session = \Stripe\Checkout\Session::create([
-    'line_items' => [[
-        'price' => $price_id,
-        'quantity' => $qty,
-    ]],
-    'metadata' => [
-        'plan_id' => $plan_id,
-        'user_id' => $user_id,
-        'role_name' => $role_name
-    ],
-    'subscription_data' => [
-        'metadata' => [
-            'plan_id' => $plan_id,
-            'user_id' => $user_id,
-            'role_name' => $role_name
-        ]
-    ],
-    //  'client_reference_id' => $conn->insert_id,
-    'mode' => 'subscription',
-    'success_url' => $_ENV['WEBSITE_HOST'] . '/plans.php?session_id={CHECKOUT_SESSION_ID}',
-    'cancel_url' => $_ENV['WEBSITE_HOST'] . '/plans.php',
-]);
+        $row = $price_data->fetch_assoc();
+        $price_id = $row['price_id'];
+        $role_name = $row['role_name'];
+        $user_id = $_SESSION['user_id'];
 
-header("HTTP/1.1 303 See Other");
-header("Location: " . $checkout_session->url);
-    
-}else{
-    header("Location: plan-view.php?error='plan not foud");
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'line_items' => [[
+                'price' => $price_id,
+                'quantity' => $qty,
+            ]],
+            'metadata' => [
+                'plan_id' => $plan_id,
+                'user_id' => $user_id,
+                'role_name' => $role_name
+            ],
+            'subscription_data' => [
+                'metadata' => [
+                    'plan_id' => $plan_id,
+                    'user_id' => $user_id,
+                    'role_name' => $role_name
+                ]
+            ],
+            //  'client_reference_id' => $conn->insert_id,
+            'mode' => 'subscription',
+            'success_url' => $_ENV['WEBSITE_HOST'] . '/plans.php?session_id={CHECKOUT_SESSION_ID}',
+            'cancel_url' => $_ENV['WEBSITE_HOST'] . '/plans.php',
+        ]);
+
+        header("HTTP/1.1 303 See Other");
+        header("Location: " . $checkout_session->url);
+
+    } else {
+        header("Location: plan-view.php?error='plan not found");
+        die();
+    }
+} else {
+    header("Location: plan-view.php?error='Required fields not found");
     die();
 }
