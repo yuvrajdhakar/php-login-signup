4b668d82ffe03ec27311b2fba949ea038733c371
Bharat Dhaker <bharatkumardhaker@gmail.com>
2022-06-17 09:39:17 +0530

    pages role..

--- add-pages.php
+++ add-pages.php
@@ -1,11 +1,6 @@
 <?php
 session_start();
 
-if ($_SESSION['role'] != 'admin') {
-    header("Location: home.php?error='You don't have permission for that page.");
-    die();
-}
-
 include "db-connection.php";
 $title = "Add new page";
 ?>
